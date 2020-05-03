c1e0f4322ddf54103c437b836d5f8eeb50335759
AndreSDS <andre_smiths@outlook.com>
2020-05-03 05:43:31 +0000

    estilizando telas login  e iniciando perfil

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const porta = 3000;
 
+app.use('/public', express.static('public'));
+
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/public/login.html')
 });
 
 app.get('/perfil', function (req, res) {
     res.sendFile(__dirname + '/public/perfil.html')
 });
 
-app.get('/error', function (req, res) {
+app.get('/**', function (req, res) {
     res.sendFile(__dirname + '/public/erro.html')
 });
 
