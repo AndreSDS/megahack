3fe92089d898b33338f34ad35f1632b993e7e16d
AndreSDS <andre_smiths@outlook.com>
2020-05-04 16:39:52 +0000

    agora sim

--- public/js/index.js
+++ public/js/index.js
@@ -43,23 +43,6 @@ window.onload = function () {
         })
     }
 
-    function createCard() {
-        card = `
-    <div class="pokemon-picture">
-      <img src="${pokemon.sprites.front_default}" alt="Sprite of ${pokemon.name}">
-    </div>
-    <div class="pokemon-info">
-        <h1 class="name">Name: ${pokemon.name}</h1>
-        <h2 class="number">Nº ${pokemon.id}</h2>
-        <h3 class="type">Type: ${pokemon.types.map(item => item.type.name).toString()}</h3>
-        <h3 class="skill">Skills: ${pokemon.moves.map(item => ' ' + item.move.name).toString()}</h3>
-        <h3 class="weight">Weight: ${pokemon.weight / 10}kg</h3>
-        <h3 class="height">Height: ${pokemon.height / 10}m</h3>
-    </div>`;
-        return card;
-    }
-
-
 
 
 
